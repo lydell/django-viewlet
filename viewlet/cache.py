72ffd934544acdb7aa2099325c9783a796f1f9fb
Simon Lydell <simon.lydell@gmail.com>
2017-04-10 14:43:41 +0200

    Try to fix Django 1.9+ support

--- viewlet/cache.py
+++ viewlet/cache.py
@@ -6,7 +6,7 @@ from .conf import settings
 def get_cache(alias=None):
     from django.core import cache
     try:
-        c = cache.get_cache(alias or settings.VIEWLET_DEFAULT_CACHE_ALIAS)
+        c = cache.caches[alias or settings.VIEWLET_DEFAULT_CACHE_ALIAS]
     except (cache.InvalidCacheBackendError, ValueError):
         c = cache.cache
     if not hasattr(c, 'clear'):  # Django < 1.2
